1c14feafe4821e72ec78d4af48d294db45125d12
Collin Sinclair <66213588+collinsinclair@users.noreply.github.com>
2022-04-01 16:15:54 -0600

    set up flask, user authentication

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,17 @@
+from os import path
+
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'csci3010'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://postgres:pwd@localhost:5432/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -15,4 +19,23 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id_):
+        return User.query.get(int(id_))
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("Database created.")
--- website/auth.py
+++ website/auth.py
@@ -1,32 +1,88 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
 
+@auth.route('/login-signup', methods=['GET', 'POST'])
+def login_signup():
+    if request.method == 'POST':
+        username = request.form.get("username")
+
+        if len(username) < 8:
+            # username was too short
+            flash("Username must be at least 8 characters.", category='error')
+
+        elif User.query.filter_by(username=username).first():
+            # user exists, sending them to login
+            return redirect(url_for('auth.login', username=username, user=current_user))
+
+        else:
+            # user is new, sending them to sign-up
+            return redirect(url_for('auth.sign_up', username=username, user=current_user))
+
+    return render_template('login-signup.html', user=current_user)
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    username = request.args.get('username')
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            # user found
+            if check_password_hash(user.password, password):
+                # user found and password correct
+                flash('You are logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                # user found and password incorrect
+                flash("Incorrect password.", category='error')
+        else:
+            # user not found
+            flash("Username not found.", category='error')
+
+    return render_template("login.html", user=current_user, username=username)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('views.home'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    username = request.args.get('username')
     if request.method == 'POST':
         username = request.form.get("username")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        if len(username) < 8:
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("username taken, please sign in or choose a different username", category='error')
+        elif len(username) < 8:
             flash('username must be at least eight (8) characters', category='error')
         elif len(password1) < 8:
             flash('password must be at least eight (8) characters', category='error')
         elif password1 != password2:
             flash('passwords do not match', category='error')
         else:
+            new_user = User(username=username, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
 
-    return render_template("sign-up.html")
+    return render_template("sign-up.html", user=current_user, username=username)
